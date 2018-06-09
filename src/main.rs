c699358616b8c6a434ab82324e5ae48bdd6360e3
Dominic Schmidt <dev@abstraqt.de>
2018-06-09 17:13:07 +0200

    Implement file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn read_json_file(path: &String) -> String {
+    println!("{}", path);
+    let mut f = File::open(path).expect("File not found");
+    let mut content = String::new();
+
+    f.read_to_string(&mut content).expect("Could not read the file");
+
+    return content
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        panic!("Not enough arguments")
+    }
+
+    let json_string = read_json_file(&args[1]);
+    println!("{}", json_string)
 }
