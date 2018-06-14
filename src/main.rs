9ecbbcaf0c4ff1f55fdd2d19e1658e905ab5967a
Dominic Schmidt <dev@abstraqt.de>
2018-06-14 23:25:26 +0200

    Migrate parsers to CompleteStr

--- src/main.rs
+++ src/main.rs
@@ -19,52 +19,59 @@ enum Value {
     Null
 }
 
-named!(string<&str, &str>,
-    delimited!(
-        tag!("\""),
-        take_until!("\""),
-        tag!("\"")
-    )
+named!(string<CompleteStr, CompleteStr>,
+//    map!(
+        delimited!(
+            char!('\"'),
+            take_until!("\""),
+            char!('\"')
+        )
+//    |s: CompleteStr| { s.as_ref() }
+//    )
 );
 
-named!(literal_true<&str, Value>,
+named!(literal_true<CompleteStr, Value>,
     value!(Value::True, tag!("true"))
 );
 
-named!(literal_false<&str, Value>,
+named!(literal_false<CompleteStr, Value>,
     value!(Value::False, tag!("false"))
 );
 
-named!(literal_null<&str, Value>,
+named!(literal_null<CompleteStr, Value>,
     value!(Value::Null, tag!("null"))
 );
-
-named!(json_value<&str, Value>,
+//
+named!(json_value<CompleteStr, Value>,
     alt!(
-        string => { |s: &str| Value::String(s.to_owned()) }
+        string => { |s: CompleteStr| Value::String(s.to_string()) }
         | literal_true
         | literal_false
         | literal_null
     )
 );
-
-named!(number<CompleteStr, Result<i32,ParseIntError>>,
-    map!(digit, |s: CompleteStr| { FromStr::from_str(s.0) })
-);
-
-named!(pair<&str, (String, Value)>,
+//
+//named!(number<CompleteStr, Result<i32,ParseIntError>>,
+//    map!(digit, |s: CompleteStr| { FromStr::from_str(s.0) })
+//);
+//
+named!(pair<CompleteStr, (String, Value)>,
     separated_pair!(
-        map!(string, |s: &str| { s.to_owned() }),
+        map!(string, |s: CompleteStr| { s.to_string() }),
         ws!(char!(':')),
         json_value
     )
 );
 
-named!(object<&str, HashMap<String, Value>>,
+named!(object<CompleteStr, HashMap<String, Value>>,
     map!(
-        separated_list_complete!(
-            ws!(char!(',')),
-            pair
+        delimited!(
+            ws!(char!('{')),
+            separated_list_complete!(
+                ws!(char!(',')),
+                pair
+            ),
+            ws!(char!('}'))
         ),
         |vec: Vec<(String, Value)>| { HashMap::from_iter(vec.into_iter()) }
     )
@@ -82,39 +89,39 @@ fn read_json_file(path: &String) -> String {
 
 #[test]
 fn parse_string() {
-    let string_test = string("\"Hallo Welt!\"");
-    assert_eq!(string_test, Ok(("", "Hallo Welt!")))
+    let string_test = string(CompleteStr("\"Hallo Welt!\""));
+    assert_eq!(string_test, Ok((CompleteStr(""), CompleteStr("Hallo Welt!"))))
 }
 
-#[test]
-fn parse_number() {
-    let number_test = number(CompleteStr("42"));
-    assert_eq!(number_test, Ok((CompleteStr(""), Ok(42))))
-}
+//#[test]
+//fn parse_number() {
+//    let number_test = number(CompleteStr("42"));
+//    assert_eq!(number_test, Ok((CompleteStr(""), Ok(42))))
+//}
 
 #[test]
 fn parse_string_pair() {
     let pair_string = "\"manufacturer\": \"BMW\"";
-    let pair_test = pair(pair_string);
-    assert_eq!(pair_test, Ok(("", ("manufacturer".to_owned(), Value::String(String::from("BMW"))))))
+    let pair_test = pair(CompleteStr(pair_string));
+    assert_eq!(pair_test, Ok((CompleteStr(""), (String::from("manufacturer"), Value::String(String::from("BMW"))))))
 }
 
 #[test]
 fn parse_literal_true() {
-    let literal_test = literal_true("true");
-    assert_eq!(literal_test, Ok(("", Value::True)))
+    let literal_test = literal_true(CompleteStr("true"));
+    assert_eq!(literal_test, Ok((CompleteStr(""), Value::True)))
 }
 
 #[test]
 fn parse_value() {
-    let value_test = json_value("null");
-    assert_eq!(value_test, Ok(("", Value::Null)))
+    let value_test = literal_null(CompleteStr("null"));
+    assert_eq!(value_test, Ok((CompleteStr(""), Value::Null)))
 }
 
 #[test]
 fn parse_object() {
-    let object_string = "\"manufacturer\": \"BMW\",\"model\": \"1 Series\", \"hatchback\": true";
-    let object_test = object(object_string);
+    let object_string = "{ \"manufacturer\": \"BMW\", \"model\": \"1 Series\", \"hatchback\": true }";
+    let object_test = object(CompleteStr(object_string));
 
     let vec = vec![
         ("manufacturer".to_string(), Value::String("BMW".to_string())),
@@ -123,7 +130,7 @@ fn parse_object() {
     ];
 
     let map: HashMap<String, Value> = HashMap::from_iter(vec.into_iter());
-    assert_eq!(object_test, Ok(("", map)))
+    assert_eq!(object_test, Ok((CompleteStr(""), map)))
 }
 
 fn main() {
